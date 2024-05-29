2cfcebcbd7ac795995e07ea27c4f30e61049c188
MD ARMAN HOSSEN <126983513+arman-12@users.noreply.github.com>
2024-05-29 13:46:26 +0600

    Add files via upload

---
+++ Tushar sir/shajid.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int i,j;
+    for(i=1;i<=9;i=i+2)
+    {
+        for(j=1;j<=5;j=j+2)
+        {
+            cout<<"I= "<<i<<" J= "<<j<<endl;
+        }
+    }
+}
